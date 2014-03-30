7e4a003c900ab2442a407b5c4856f165d5dab7b8
James Aspinwall <jamesaspinwall@gmail.com>
2014-03-30 19:28:07 -0400

    initial rails bigdata

---
+++ bigdata/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bigdata::Application.config.secret_key_base = '613f70c274b7adf2b974c12612f58e75eca40850b8c6688c3dea9ee4ca44e71f5b81a76c2a2358feff8503e9f9b8b88426ad45e03a57dee3f9d21a30e32a0680'
